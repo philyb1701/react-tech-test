0575a94449fe44be3b5ecdcc14a3d27551b95b10
Phil Burkhill <philip.burkhill@gmail.com>
2022-06-20 20:45:06 +0100

    app and search tests added

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,2 +1,11 @@
-import { render, screen } from "@testing-library/react";
+import React from "react";
+import { render } from "@testing-library/react";
 import App from "../components/App";
+
+describe("App", () => {
+  const { asFragment } = render(<App />);
+
+  it("renders correctly", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+});
