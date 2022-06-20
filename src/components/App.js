cc7b632a485a248615c0e220a50730ce8e46d46d
Phil Burkhill <philip.burkhill@gmail.com>
2022-06-20 20:34:52 +0100

    search bar added.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,16 @@
 import React from "react";
+import Search from "./Search";
 import "../styles/app.css";
 
 const App = () => {
   return (
     <div className="app">
-      <p>Hello World!</p>
+      <img
+        className="nasa-logo"
+        src="https://cdn.cnn.com/cnnnext/dam/assets/200424060716-nasa-worm-logo.jpg"
+        alt="nasa-logo"
+      />
+      <Search />
     </div>
   );
 };
---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "../styles/search.css";
+
+function Search() {
+  return (
+    <>
+      <input className="search-bar" type="text" />
+    </>
+  );
+}
+
+export default Search;
