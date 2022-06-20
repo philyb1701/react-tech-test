b6bfe5f46390948868bdd1375b671d1639532f91
Phil Burkhill <philip.burkhill@gmail.com>
2022-06-20 22:57:40 +0100

    can now query api for images

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
+import getImages from "../requests/getImages";
 import "../styles/search.css";
 
 function Search() {
   const [value, setValue] = useState();
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    getImages(value);
+  };
+
   return (
-    <>
-      <form>
-        <input
-          className="search-bar"
-          type="text"
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <button className="search-button" type="submit">
-          Search...
-        </button>
-      </form>
-    </>
+    <form className="search-form" onSubmit={handleSubmit}>
+      <input
+        className="search-bar"
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button className="search-button" type="submit">
+        Search...
+      </button>
+    </form>
   );
 }
 
